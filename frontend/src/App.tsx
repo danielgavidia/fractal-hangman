e4392d9e86cdff30ba3464c0e8104bdb66c08e48
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-02 14:06:02 -0400

    Added basic win-lose functionality, with hangman figurine render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { alphabet } from "./components/alphabet";
 
 import Letter from "./components/Letter";
 import Keyboard from "./components/Keyboard";
+import { hangmanStages } from "./components/hangman";
 
 // convert word to array of {letter: string, shown: boolean}
 const getLetterArray = (word: string) => {
@@ -21,11 +22,24 @@ const getKeyboardLetterArray = (alphabet: string[]) => {
 	return arr;
 };
 
+// convert hangman array to array of {stage: number, figure: string}
+const getHangmanArray = (hangmanStages: string[]) => {
+	const arr = hangmanStages.map((x, index) => ({
+		stage: index,
+		figure: x,
+	}));
+	return arr;
+};
+
 const App = () => {
 	const word = "kaleidoscope";
+	const maxWrong = 6;
 	const [lettersArr, setLettersArr] = useState(getLetterArray(word));
 	const [keyboardLetters, setKeyboardLetters] = useState(getKeyboardLetterArray(alphabet));
-	console.log(keyboardLetters);
+	const [wrongMoveCount, setWrongMoveCount] = useState<number>(0);
+	const [lost, setLost] = useState<boolean>(false);
+	const [won, setWon] = useState<boolean>(false);
+	const hangmanArray = getHangmanArray(hangmanStages);
 
 	// logic for showing letters
 	const handleKeyboardLetter = (letter: string): void => {
@@ -60,9 +74,36 @@ const App = () => {
 		});
 		setKeyboardLetters(newKeyboardLetters);
 	};
+
+	// useEffect for updating moveCount and win-lose
+	useEffect(() => {
+		// losing functionality
+		const wrongMoves = keyboardLetters.filter((x) => !x.correct);
+		setWrongMoveCount(wrongMoves.length);
+		if (wrongMoves.length === maxWrong) {
+			setLost(true);
+		}
+
+		// winning functionality
+		const correctLetters = lettersArr.filter((x) => x.shown === true);
+		if (correctLetters.length === lettersArr.length) {
+			setWon(true);
+		}
+	}, [keyboardLetters]);
+
 	return (
 		<div>
 			<br />
+			<div>Won game: {won.toString()}</div>
+			<div>Lost game: {lost.toString()}</div>
+			<div>Max # of wrong moves allowed: {maxWrong}</div>
+			<div>Current # of wrong moves: {wrongMoveCount}</div>
+			<div>
+				<div>The Hangman</div>
+				<pre className="font-mono">
+					{hangmanArray.find((x) => x.stage === wrongMoveCount)?.figure}
+				</pre>
+			</div>
 			<div className="flex justify-between">
 				{lettersArr.map((x, index) => {
 					return <Letter key={index} letter={x.letter} shown={x.shown} />;
