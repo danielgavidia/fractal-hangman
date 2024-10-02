3a9eff4f5b9f933a04ece2fc46c25b9a2c270244
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-02 12:27:16 -0400

    Added initial array with alphabet buttons and Letter component that hides letter with boolean

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,42 @@
-// const word = "kaleidoscope"
+import { useState } from "react";
+
+import Letter from "./components/Letter";
+import Keyboard from "./components/Keyboard";
+
+// convert word to array of {letter: string, shown: boolean}
+const getLetterArray = (word: string) => {
+	const arr = word.split("");
+	const arrObj = arr.map((x) => ({ letter: x, shown: false }));
+	return arrObj;
+};
 
 const App = () => {
-    return (
-        <div className="bg-red-100">App</div>
-    )
-}
+	const word = "kaleidoscope";
+	const [lettersArr, setLettersArr] = useState(getLetterArray(word));
 
-export default App
+	// logic for showing letters
+	const showLetter = (letter: string): void => {
+		const newLetters = lettersArr.map((x) => {
+			if (x.letter === letter) {
+				return { ...x, shown: true };
+			} else {
+				return x;
+			}
+		});
+		setLettersArr(newLetters);
+	};
+	return (
+		<div>
+			<br />
+			<div className="flex justify-between">
+				{lettersArr.map((x, index) => {
+					return <Letter key={index} letter={x.letter} shown={x.shown} />;
+				})}
+			</div>
+			<br />
+			<Keyboard showLetter={showLetter} />
+		</div>
+	);
+};
+
+export default App;
