e9cd58b25de573d9417510ef8168302e49aa45b5
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-18 18:29:40 -0400

    Deployment fix frontend

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,7 +1,10 @@
 import { io, Socket } from "socket.io-client";
 
+// Determine the correct protocol and URL based on the environment
 export const socket: Socket = io(
-	window.location.hostname === "localhost" ? "http://localhost:3000" : "http://fractal-hangman.com"
+	window.location.hostname === "localhost"
+		? "http://localhost:3000" // Use ws:// for development
+		: `wss://${window.location.hostname}` // Use wss:// for production
 );
 
 // export const socket: Socket = io(import.meta.env.VITE_BACKEND_URL);
